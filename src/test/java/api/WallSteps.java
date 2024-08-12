aead583822d6839c82871e6c5b7b9957f91cdbc8
EvgeniyaGlukhikh <evgeniya8787@mail.ru>
2024-08-12 12:48:21 +0300

    commit

--- src/test/java/api/WallSteps.java
+++ src/test/java/api/WallSteps.java
@@ -3,22 +3,16 @@ package api;
 import constant.ApiPathConstants;
 import constant.UserIdConstant;
 import constant.WallPostConstant;
-import dev.failsafe.internal.util.Assert;
-import io.restassured.http.ContentType;
-import org.hamcrest.Matchers;
+import io.restassured.RestAssured;
+import org.apache.hc.core5.http.HttpStatus;
 import provider.ConfigProviders;
 import provider.DataProviders;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static io.restassured.RestAssured.given;
-import static org.apache.http.HttpStatus.SC_OK;
 
 public class WallSteps {
 
     public static void createPost(String message) {
-        given()
+        RestAssured.given()
                 .relaxedHTTPSValidation()  // для решения проблемы с сертификатами (HTTPS)
                 .baseUri(DataProviders.API_URL)
                 .basePath("/wall.post")
@@ -26,13 +20,15 @@ public class WallSteps {
                 .param("message", message)
                 .param("access_token", ApiPathConstants.TOKEN)
                 .param("v", ConfigProviders.API_VERSION)
-                .contentType(ContentType.JSON)
+                .contentType(io.restassured.http.ContentType.JSON)
                 .when().post()
-                .then().statusCode(SC_OK).log();
+                .then().statusCode(HttpStatus.SC_OK).log();
     }
 
+
+
     public static void createPostWithPhoto(String message, String attachment) {
-        given()
+        RestAssured.given()
                 .relaxedHTTPSValidation()
                 .baseUri(DataProviders.API_URL)
                 .basePath("/wall.post")
@@ -41,22 +37,22 @@ public class WallSteps {
                 .param("attachment", attachment)
                 .param("access_token", ApiPathConstants.TOKEN)
                 .param("v", ConfigProviders.API_VERSION)
-                .contentType(ContentType.JSON)
+                .contentType(io.restassured.http.ContentType.JSON)
                 .when().post()
-                .then().statusCode(SC_OK).log();
+                .then().statusCode(HttpStatus.SC_OK).log();
     }
 
     public static void getTextFromPost() {
-        given()
+        RestAssured.given()
                 .relaxedHTTPSValidation()
                 .baseUri(DataProviders.API_URL)
                 .basePath("/wall.get")
                 .param("owner_id", UserIdConstant.UserID)
                 .param("access_token", ApiPathConstants.TOKEN)
                 .param("v", ConfigProviders.API_VERSION)
-                .contentType(ContentType.JSON)
+                .contentType(io.restassured.http.ContentType.JSON)
                 .when().get(WallPostConstant.FIRST_POST_TEXT)
-                .then().statusCode(SC_OK).log();
+                .then().statusCode(HttpStatus.SC_OK).log();
     }
 
 }
